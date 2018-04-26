0dc0d5d1f16a911813a92aa384d35cbe63db93b3
anjalisg <anjalisgopinathan@gmail.com>
2018-04-26 11:19:39 -0700

    worked on starfighter boundaries

---
+++ Ship.java
@@ -0,0 +1,71 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class - 
+//Lab  -
+
+import java.io.File;
+import java.net.URL;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.imageio.ImageIO;
+
+public class Ship extends MovingThing {
+	private int speed;
+	private Image image;
+
+	public Ship() {
+		this(10,10,10,10,10);
+	}
+
+	public Ship(int x, int y) {
+	   super(x,y);
+	}
+
+	public Ship(int x, int y, int s) {
+		super(x,y);
+		speed = s;
+	}
+
+	public Ship(int x, int y, int w, int h, int s) {
+		super(x, y, w, h);
+		speed=s;
+		try {
+			image = ImageIO.read(new File("./images/ship.jpg"));
+		}
+		catch(Exception e) {
+			System.out.println("problem reading ship image");
+			e.printStackTrace();
+		}
+	}
+
+
+	public void setSpeed(int s) {
+	   speed = s;
+	}
+
+	public int getSpeed() {
+	   return speed;
+	}
+
+	public void move(String direction) {
+		if(direction.equals("UP"))
+			setY(getY() - getSpeed());
+		else if(direction.equals("DOWN"))
+			setY(getY() + getSpeed());
+		else if(direction.equals("RIGHT"))
+			setX(getX()+getSpeed());
+		else if(direction.equals("LEFT"))
+			setX(getX()-getSpeed());
+		
+	}
+
+	public void draw( Graphics window ) {
+		window.drawImage(image,getX(),getY(),getWidth(),getHeight(),null);
+	}
+
+	public String toString() {
+		return super.toString() +" "+ getSpeed();
+	}
+}
